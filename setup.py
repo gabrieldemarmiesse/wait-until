6244ff16da2c6e85b13523892c77ec248e3467b3
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-06-28 14:32:47 +0000

    Updated version.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name="wait-until", packages=find_packages(), version="0.1")
+setup(name="wait-until", packages=find_packages(), version="0.2")
